d45db80965bb8bef0ad74b0ff949d2f5de491561
Vlad Brok <vlad.brok99@gmail.com>
2022-05-04 10:33:14 +0300

    Remove useless comments

--- src/Navigatio/Program.cs
+++ src/Navigatio/Program.cs
@@ -5,7 +5,6 @@ using static System.IO.Path;
 
 // TODO:
 // add windows cmd support
-// optimize file IO
 // make history limit ?
 
 string exePath = AppContext.BaseDirectory;
@@ -18,10 +17,4 @@ var history = new History(historyStorage);
 var table = new Table();
 
 var commander = new Commander(aliases, history, table, shellFile: args[0]);
-
-var watch = Stopwatch.StartNew();
 commander.Run(args[1..]);
-
-// Average execution time: 1000 ms
-// Looks like the History is a bottleneck
-Console.WriteLine($"Executed in {watch.ElapsedMilliseconds} ms.");
