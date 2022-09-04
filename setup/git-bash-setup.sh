b732fdbbf750c39c0987453ac8888661a72e44ad
Vlad Brok <vlad.brok99@gmail.com>
2022-09-04 20:44:39 +0300

    Uncomment code for creating an alias

--- setup/git-bash-setup.sh
+++ setup/git-bash-setup.sh
@@ -8,7 +8,6 @@ cp nav.sh $ROOT
 cp -r ${EXE_PATH}. $ROOT
 chmod +x ${ROOT}/nav.sh
 
-# Uncomment to create an alias
-#echo "" >> ~/.bashrc
-#echo "alias nav='source ${ROOT}/nav.sh ${ROOT}/'" >> ~/.bashrc
-#source ~/.bashrc
+echo "" >> ~/.bashrc
+echo "alias nav='source ${ROOT}/nav.sh ${ROOT}/'" >> ~/.bashrc
+source ~/.bashrc
