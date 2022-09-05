661ad6d43ad7228abff1bf5e0b4665a607714d5d
Vlad Brok <vlad.brok99@gmail.com>
2022-09-05 12:29:48 +0300

    Add scripts directory

--- dev-build.sh
+++
@@ -1,1 +0,0 @@
-dotnet build src/Navigatio --no-restore && source ./setup.sh src/navigatio/bin/debug/net6.0/
---
+++ scripts/dev-build.sh
@@ -0,0 +1,1 @@
+dotnet build src/navigatio --no-restore && source scripts/setup.sh src/navigatio/bin/debug/net6.0/
