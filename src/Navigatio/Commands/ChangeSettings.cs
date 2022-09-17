81d916a77c01180f3f5fba6076d3ab8899317bd6
Vlad Brok <vlad.brok99@gmail.com>
2022-09-17 19:24:33 +0300

    Determine a default editor based on OS

--- src/Navigatio/Commands/ChangeSettings.cs
+++ src/Navigatio/Commands/ChangeSettings.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Navigatio.Storages;
+using System.Runtime.InteropServices;
 
 namespace Navigatio.Commands;
 
@@ -17,10 +18,22 @@ public class ChangeSettings : IExecutable, ICancellable
 
     public bool Execute(params string[] _)
     {
-        string editor = Settings.FavoriteEditor ?? GetDefaultEditor();
+        string editor = "";
+        try
+        {
+            editor = Settings.FavoriteEditor ?? GetDefaultEditor();
+        }
+        catch (PlatformNotSupportedException)
+        {
+            Console.WriteLine(
+                $"Sorry, your OS is currently not supported. Please open file {_storage.File} and edit it manually."
+            );
+            return false;
+        }
+
         if (Settings.FavoriteEditor is null)
         {
-            Console.WriteLine($"Favorite editor is not specified. Falling back to {editor}.");
+            Console.WriteLine($"Note: favorite editor is not specified. Falling back to {editor}.");
         }
 
         Process? editorInstance;
@@ -54,6 +67,17 @@ public class ChangeSettings : IExecutable, ICancellable
 
     private string GetDefaultEditor()
     {
-        return "notepad";
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            return "vi";
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            return "notepad";
+        }
+        else
+        {
+            throw new PlatformNotSupportedException();
+        }
     }
 }
