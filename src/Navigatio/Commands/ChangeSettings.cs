4fe94ee7da3fb953f4fe531ef64e82e53464bb54
Vlad Brok <vlad.brok99@gmail.com>
2022-09-17 18:35:26 +0300

    Set FavoriteEditor to null initially

--- src/Navigatio/Commands/ChangeSettings.cs
+++ src/Navigatio/Commands/ChangeSettings.cs
@@ -9,33 +9,34 @@ public class ChangeSettings : IExecutable, ICancellable
 
     public ChangeSettings(Settings current, IStorage<Settings> storage)
     {
-        OldSettings = current;
+        Settings = current;
         _storage = storage;
     }
 
-    public Settings OldSettings { get; set; }
+    public Settings Settings { get; set; }
 
     public bool Execute(params string[] _)
     {
-        Process? editor;
+        string editor = Settings.FavoriteEditor ?? GetDefaultEditor();
+        if (Settings.FavoriteEditor is null)
+        {
+            Console.WriteLine($"Favorite editor is not specified. Falling back to {editor}.");
+        }
+
+        Process? editorInstance;
         try
         {
-            var info = new ProcessStartInfo(OldSettings.FavoriteEditor ?? "", _storage.File)
-            {
-                Verb = "Edit"
-            };
-            editor = Process.Start(info) ?? throw new NullReferenceException();
+            var info = new ProcessStartInfo(editor, _storage.File) { Verb = "Edit" };
+            editorInstance = Process.Start(info) ?? throw new NullReferenceException();
         }
         catch
         {
-            Console.WriteLine(
-                $"Failed to open your favorite editor ({OldSettings.FavoriteEditor})."
-            );
+            Console.WriteLine($"Failed to open your favorite editor ({editor}).");
             return false;
         }
 
         Console.WriteLine("Waiting for your editor to exit...");
-        editor?.WaitForExit();
+        editorInstance?.WaitForExit();
         Console.WriteLine("Settings are changed.");
         return true;
     }
@@ -45,9 +46,14 @@ public class ChangeSettings : IExecutable, ICancellable
         _storage.Load(
             settings =>
             {
-                OldSettings.ShallowCopyTo(settings);
+                Settings.ShallowCopyTo(settings);
                 Console.WriteLine("Settings are changed.");
             }
         );
+    }
+
+    private string GetDefaultEditor()
+    {
+        return "notepad";
     }
 }
