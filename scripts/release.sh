d81b08771504ec09cf205fe49dc9ca2328ce403c
Vlad Brok <vlad.brok99@gmail.com>
2022-09-17 18:45:33 +0300

    Adjust a release.sh script

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,10 +3,10 @@
 PROJECT_DIR=src/navigatio
 BASE_DIR=${PROJECT_DIR}/bin/Release
 
-rm -rf ${BASE_DIR}/x86 && \
-dotnet publish $PROJECT_DIR -c Release -o ${BASE_DIR}/x86 -a x86 --sc true && \
-powershell Compress-Archive -Path "'${BASE_DIR}/x86/*'" -DestinationPath 'windows-x86.zip' -Force
+rm -rf ${BASE_DIR}/win-x86 && \
+dotnet publish $PROJECT_DIR --configuration Release --output ${BASE_DIR}/win-x86 --runtime win-x86 --self-contained true && \
+powershell Compress-Archive -Path "'${BASE_DIR}/win-x86/*'" -DestinationPath 'win-x86.zip' -Force
 
-rm -rf ${BASE_DIR}/x64 && \
-dotnet publish $PROJECT_DIR -c Release -o ${BASE_DIR}/x64 -a x64 --sc true && \
-powershell Compress-Archive -Path "'${BASE_DIR}/x64/*'" -DestinationPath 'windows-x64.zip' -Force
+rm -rf ${BASE_DIR}/win-x64 && \
+dotnet publish $PROJECT_DIR --configuration Release --output ${BASE_DIR}/win-x64 --runtime win-x64 --self-contained true && \
+powershell Compress-Archive -Path "'${BASE_DIR}/win-x64/*'" -DestinationPath 'win-x64.zip' -Force
